6fc9ada42de6948261b3478bfa8630035346ceab
lvhuaisheng <lvhuaisheng_k@163.com>
2022-06-30 15:34:16 +0800

    docs：增加预览地址

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import {resolve} from 'path'
+import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  plugins: [
-      createSvgIconsPlugin({
-          // 指定需要缓存的图标文件夹
-          iconDirs: [resolve(process.cwd(), 'src/icons/svg')],
-          // 指定symbolId格式
-          symbolId: 'icon-[name]',
-      }),
-      vue()]
-})
+        plugins: [
+            createSvgIconsPlugin({
+                // 指定需要缓存的图标文件夹
+                iconDirs: [resolve(process.cwd(), 'src/icons/svg')],
+                // 指定symbolId格式
+                symbolId: 'icon-[name]',
+            }),
+            vue()],
+        /**
+         * 在生产中服务时的基本公共路径。
+         * @default '/'
+         */
+        base: '/naiveui-form-generate',
+    },
+)
